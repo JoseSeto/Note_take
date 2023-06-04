6ce133c8f0ae0847e955212da54577385f887f99
Jose Seto <joseseto8@gmail.com>
2023-06-04 13:06:01 -0700

    added middleware and HTML routes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -4,3 +4,20 @@ const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
