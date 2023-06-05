43ce712e20e0ec6a744f5a82889215d767aadf56
Jose Seto <joseseto8@gmail.com>
2023-06-05 08:44:45 -0700

    added routes and moved server.js from public

---
+++ Develop/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//API routes
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+  let db = fs.readFileSync('db/db.json');
+  db = JSON.parse(db);
+  res.json(db);
+  // creating body for note
+  let userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    // creating unique id for each note
+    id: uniqid(),
+  };
+  // pushing created note to be written in the db.json file
+  db.push(userNote);
+  fs.writeFileSync('db/db.json', JSON.stringify(db));
+  res.json(db);
+
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  // reading notes form db.json
+  let db = JSON.parse(fs.readFileSync('db/db.json'))
+  // removing note with id
+  let deleteNotes = db.filter(item => item.id !== req.params.id);
+  // Rewriting note to db.json
+  fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
+  
+})
+
+// HTML routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+  });
+  
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../notes.html'));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
