085d146547d93b066a2476f5d2a9875cd6410882
Jose Seto <joseseto8@gmai.com>
2023-06-04 17:15:03 -0700

    Added get route for api

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -9,6 +9,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+//API routes
+app.get('/api/notes', (req,res) => {
+  fs.readFile(path.join (__dirname, '/db/db.json'), 'utf-8',
+  (err, data) =>{
+    if (err){
+      console.error(err);
+      return res.status(500).json({ error: 'Failed to read notes'})
+    }
+    res.json(JSON.parse(data))
+  })
+})
+
 // HTML routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
